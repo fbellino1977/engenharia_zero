95f8d3595428b84973dabb1b02a9b0be4d43b9e6
Fabio Bellino <br.sp.fb.ti@gmail.com>
2026-04-17 23:30:17 -0300

    style: format tests/test_api.py with ruff

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,20 +3,20 @@ from main import app
 
 client = TestClient(app)
 
+
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Engenharia Zero API ativa e conectada ao banco"}
+    assert response.json() == {
+        "message": "Engenharia Zero API ativa e conectada ao banco"
+    }
+
 
 def test_create_user():
     # We tested creating a new user
     response = client.post(
         "/users/",
-        json={
-            "id": 0,
-            "name": "Test User", 
-            "age": 25, 
-            "email": "test@api.com"}
+        json={"id": 0, "name": "Test User", "age": 25, "email": "test@api.com"},
     )
     assert response.status_code == 201
     assert response.json()["name"] == "Test User"
